830f86aacb234b01f5a56812ad89f1381aa45e0b
luuquy <luuquy6a@gmail.com>
2020-04-26 10:55:44 +0700

    newGame

--- app.js
+++ app.js
@@ -10,16 +10,29 @@ GAME RULES:
 */
 var scores, roundScore, activePlayer, dice;
 
-scores = [0, 0];
-roundScore = 0;
-activePlayer = 0;
+function newGame() {
+  scores = [0, 0];
+  roundScore = 0;
+  activePlayer = 0;
 
-document.querySelector('.dice').style.display = 'none';
+  document.querySelector('.dice').style.display = 'none';
 
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
+  document.getElementById('score-0').textContent = '0';
+  document.getElementById('score-1').textContent = '0';
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+
+  document.querySelector('#name-0').textContent = 'Player 1';
+  document.querySelector('#name-1').textContent = 'Player 2';
+
+  document.querySelector('.player-0-panel').classList.remove('winner');
+  document.querySelector('.player-1-panel').classList.remove('winner');
+  document.querySelector('.player-0-panel').classList.remove('active');
+  document.querySelector('.player-1-panel').classList.remove('active');
+  document.querySelector('.player-0-panel').classList.add('active');
+}
+
+newGame();
 
 
 document.querySelector('.btn-roll').addEventListener('click', function () {
@@ -84,6 +97,8 @@ function nextPlayer() {
 
   document.querySelector('.dice').style.display = 'none';
 }
+
+document.querySelector('.btn-new').addEventListener('click', newGame);
 
 
 
